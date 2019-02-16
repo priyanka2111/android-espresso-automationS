d0002df314d5162150462cb78efbe8b52495caae
vikash_patel <vikash.b.patel@infostrech.com>
2019-02-16 13:16:27 +0530

    UiAutomationSteps Updated

--- app/src/androidTest/java/com/infostretch/android/steps/UiAutomatorSteps.java
+++ app/src/androidTest/java/com/infostretch/android/steps/UiAutomatorSteps.java
@@ -1,8 +1,15 @@
 package com.infostretch.android.steps;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.BySelector;
 import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.Until;
 
+import com.infostretch.android.core.AutomationCore;
 import com.infostretch.android.core.NoSuchElementFoundException;
 import com.infostretch.android.utils.LocatorUtils;
 
@@ -30,6 +37,53 @@ public class UiAutomatorSteps {
             throw new NoSuchElementFoundException(e);
         }
     }
+
+
+    public static void presenceOfElementLocated(String loc) throws Exception {
+        presenceOfElementLocated(loc, 5000);
+    }
+
+    public static void presenceOfElementLocated(String loc, long timeout) throws Exception {
+
+        BySelector byselector = getBySelector(loc);
+        AutomationCore.getInstance().getUiDevice().wait(Until.hasObject(byselector), timeout);
+    }
+
+
+    private static BySelector getBySelector(String loc) throws Exception {
+        Context context = AutomationCore.getInstance().getInstrumentation().getContext();
+        Resources resources = context.getResources();
+        int id = resources.getIdentifier(loc, "string", context.getPackageName());
+        String value = resources.getString(id);
+        String[] arrOfValue = value.split("=", 2);
+        String prefix = arrOfValue[0];
+        String suffix = arrOfValue[1];
+        return getSelector(prefix, suffix);
+    }
+
+    private static BySelector getSelector(String strategy, String text) throws Exception {
+
+        String s = strategy.toUpperCase();
+        ;
+        BySelector selector = null;
+        switch (s) {
+            case "CLASSNAME":
+                selector = By.clazz(text);
+                break;
+            case "TEXT":
+                selector = By.text(text);
+                break;
+            case "ID":
+                selector = By.res(text);
+                break;
+            case "CONTDESC":
+            case "ACCESSIBILITY_ID":
+                selector = By.desc(text);
+                break;
+        }
+        return selector;
+    }
+
 
     public static UiObject getUIElement(String loc) {
         return LocatorUtils.getUIViewLocator(loc);
