eee77939b5fb0efe1209c699a3da1c310302d85a
priyanka.devaliya <priyanka.devaliya@infostretch.com>
2019-02-18 15:17:46 +0530

    EspressoSteps file is updated

--- app/src/androidTest/java/com/infostretch/android/steps/EspressoSteps.java
+++ app/src/androidTest/java/com/infostretch/android/steps/EspressoSteps.java
@@ -1,13 +1,25 @@
 package com.infostretch.android.steps;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.BySelector;
+import android.support.test.uiautomator.Until;
+import android.view.View;
 
+import com.fastaccess.ui.modules.repos.issues.issue.details.IssuePagerMvp;
+import com.infostretch.android.core.AutomationCore;
 import com.infostretch.android.utils.LocatorUtils;
+
+import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.isEnabled;
 
 public class EspressoSteps {
 
@@ -17,7 +29,6 @@ public class EspressoSteps {
 
     public static void sendKeys(String loc, String text) {
         sendKeys(loc,text,false);
-
     }
 
     public static void sendKeys(String loc, String text, boolean hideKeyboard) {
@@ -27,9 +38,99 @@ public class EspressoSteps {
             getElement(loc).perform(ViewActions.typeText(text));
         }
     }
+    public static void clearText(String loc){
+        getElement(loc).perform(ViewActions.clearText());
+    }
+
+    public static void TypeText(String loc ,String text){
+        getElement(loc).perform(ViewActions.typeText(text));
+    }
+
+    public static void DoubleClick(String loc){
+        getElement(loc).perform(ViewActions.doubleClick());
+    }
+
+    public static void LongClick(String loc){
+        getElement(loc).perform(ViewActions.longClick());
+    }
+
+
+    public static void ScrollTo(String loc){
+        getElement(loc).perform(ViewActions.scrollTo());
+    }
+
+    public static void OpenLinkWithText(String loc ,String linkText){
+        getElement(loc).perform(ViewActions.openLinkWithText(linkText));
+    }
+
+    public static void OpenLinkWithURI(String loc,String uri){
+        getElement(loc).perform(ViewActions.openLinkWithUri(uri));
+    }
+
+    public static void ReplaceText(String loc,String text){
+        getElement(loc).perform(ViewActions.replaceText(text));
+    }
+
+    public static void IsEnabled(String loc){
+        getElement(loc).check(matches(ViewMatchers.isEnabled()));
+    }
+
+    public static void IsDisplayed(String loc){
+        getElement(loc).check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    public static void IsChecked(String loc){
+        getElement(loc).check(matches(ViewMatchers.isChecked()));
+    }
+
+    public static void IsNotChecked(String loc){
+        getElement(loc).check(matches(ViewMatchers.isNotChecked()));
+    }
+
+    public static void IsSelected(String loc){
+        getElement(loc).check(matches(ViewMatchers.isSelected()));
+    }
+
+    public static void HasContentDescription(String loc){
+        getElement(loc).check(matches(ViewMatchers.hasContentDescription()));
+    }
+
+    public static void WithContentDescription(String loc,String text){
+        getElement(loc).check(matches(ViewMatchers.withContentDescription(text)));
+    }
+
+    public static void WithHint(String loc,String hintText){
+        getElement(loc).check(matches(ViewMatchers.withHint(hintText)));
+    }
+
+    public static void WithText(String loc, String text){
+        getElement(loc).check(matches(ViewMatchers.withText(text)));
+    }
+
+    public static void WithID(String loc ,int id){
+        getElement(loc).check(matches(ViewMatchers.withId(id)));
+    }
+
+    public static void WithClassName(String loc ,Matcher<String> stringMatcher){
+        getElement(loc).check(matches(ViewMatchers.withClassName(stringMatcher)));
+    }
+
+    public static void WithTagKey(String loc ,int id){
+        getElement(loc).check(matches(ViewMatchers.withTagKey(id)));
+    }
+    public static void WithTagValue(String loc , Matcher<Object> objectMatcher ){
+        getElement(loc).check(matches(ViewMatchers.withTagValue(objectMatcher)));
+    }
 
 
     public static ViewInteraction getElement(String loc){
         return LocatorUtils.getViewLocator(loc);
     }
+
 }
+
+
+
+
+
+
